3001fcdb5db7348c1b5c3be60cd430685ab4c51f
AkmalOt <akmalotambekov@gmail.com>
2023-04-26 18:04:18 +0500

    Updated Main.go

--- main.go
+++ main.go
@@ -4,8 +4,19 @@ import (
 	"awesomeProject2/Handler"
 	"awesomeProject2/db"
 	"awesomeProject2/repository"
+	"awesomeProject2/repository/AccAgent"
+	"awesomeProject2/repository/AgentsTable"
 	"awesomeProject2/repository/Country"
+	"awesomeProject2/repository/CurrencyTable"
 	"awesomeProject2/repository/Language"
+	PaymentType "awesomeProject2/repository/PaymentTypeTable"
+	ServCountry "awesomeProject2/repository/ServCountryTable"
+	ServiceRules "awesomeProject2/repository/ServiceRulesTable"
+	"awesomeProject2/repository/ServiceTable"
+	SysMessage "awesomeProject2/repository/SysMessageTable"
+	TransferTable "awesomeProject2/repository/TransfersTable"
+	"awesomeProject2/repository/UserInfoTable"
+	"awesomeProject2/repository/VendorTable"
 	"github.com/gin-gonic/gin"
 	"log"
 )
@@ -13,10 +24,24 @@ import (
 func main() {
 	route := gin.Default()
 	DB := db.SetupGorm()
+	if DB.Error != nil {
+		return
+	}
 
 	repoLayer := &repository.Repository{
-		CountryRepo:  Country.CountryRepo{Connection: DB},
-		LanguageRepo: Language.LanguageRepo{Connection: DB},
+		AccAgentRepo:      AccAgent.AccAgentRepo{},
+		AgentsTableRepo:   AgentsTable.AgentsTableRepo{},
+		CountryRepo:       Country.CountryRepo{},
+		CurrencyTableRepo: CurrencyTable.CurrencyTableRepo{},
+		LanguageRepo:      Language.LanguageRepo{},
+		PaymentTypeRepo:   PaymentType.PaymentTypeRepo{},
+		ServCountryRepo:   ServCountry.ServCountryRepo{},
+		ServiceRulesRepo:  ServiceRules.ServiceRulesRepo{},
+		ServiceTableRepo:  ServiceTable.ServiceTableRepo{},
+		SysMessageRepo:    SysMessage.SysMessageRepo{},
+		TransferTableRepo: TransferTable.TransferTableRepo{},
+		UserInfoRepo:      UserInfoTable.UserInfoRepo{},
+		VendorRepo:        VendorTable.VendorRepo{},
 	}
 	newHandler := Handler.NewHandler(route, repoLayer)
 	//newHandler := routs.NewHandler(route, handler)
